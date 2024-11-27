15a073e0e2e963c4b658aed229b6fb96497fa923
Liubomyr Skorokhod <lyubko44@gmail.com>
2024-11-26 16:41:21 -0800

    implement step 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,13 +17,21 @@ const App = () => {
         }));
     };
 
+    const resetFeedback = () => {
+        setFeedback({
+            good: 0,
+            neutral: 0,
+            bad: 0
+        });
+    };
+
     const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
     return (
         <div>
             <h1>Sip Happens Café</h1>
             <p>Please leave your feedback about our service by selecting one of the options below.</p>
-            <Options updateFeedback={updateFeedback} />
+            <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
             {totalFeedback > 0 ? (
                 <Feedback feedback={feedback} />
             ) : (
